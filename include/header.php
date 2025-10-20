c032adf5cb56c110b7072cd7b927a732a6ea5f94
songhun115 <hoho77113@daum.com>
2025-10-20 14:13:07 +0900

    타입형서버에 업로드 파일로 전부 대체

---
+++ include/header.php
@@ -0,0 +1,23 @@
+<header id="header">
+        <div class="header-wrap">
+            <div class="logo">
+                <a href="index.html"><img src="img/common/logo.png" alt=""></a>
+            </div>
+            <nav class="nav">
+                <ul>
+                    <li>
+                        <a href="sub1_tour.html">풍경</a>
+                    </li>
+                    <li>
+                        <a href="sub2_room1.html">머무르다</a>
+                    </li>
+                    <li>
+                        <a href="#">마련하다</a>
+                    </li>
+                    <li>
+                        <a href="#">예약안내</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>

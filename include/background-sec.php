2fe8912a6ec1d485743b413e7aa5b675666093ad
songhun115 <hoho77113@daum.com>
2025-10-30 16:29:34 +0900

    수정사항최종반영

---
+++ include/background-sec.php
@@ -0,0 +1,9 @@
+ <section class="background-sec">
+            <div class="box">
+                <div class="thumbnail" data-aos="fade-up" data-aos-easing="ease-out-cubic" data-aos-duration="1000" data-aos-delay="100"><img src="img/background-img.png" alt=""></div>
+                <div class="txt" data-aos="fade-up" data-aos-easing="ease-out-cubic" data-aos-duration="1000" data-aos-delay="100">
+                    <p>일상의 소란에서 벗어나 휴식의 시간을 만끽하세요. <br>
+                        오롯이 당신만을 위해 준비된 공간, 유연(悠然)입니다.</p>
+                </div>
+            </div>
+        </section>
